5e62240734f54f25bf8ef76fec0750e6adef5ca9
Maria Morozova <marommv9@gmail.com>
2023-11-29 17:54:28 +0300

    Adding exsisting files

---
+++ labs/lab08/report/lab8-4.asm
@@ -0,0 +1,37 @@
+%include 'in_out.asm'
+ 
+SECTION .data
+f_x db "функция: 3(x+2)",0h
+msg db 10,13,'результат: ',0h
+ 
+SECTION .text
+global _start
+ 
+_start:
+pop ecx
+pop edx
+sub ecx,1
+mov esi, 0
+ 
+next:
+cmp ecx,0h
+jz _end
+pop eax
+call atoi
+;dec eax
+add eax,2
+mov ebx,3
+mul ebx
+add esi, eax
+ 
+loop next
+ 
+_end:
+mov eax, f_x
+call sprint
+mov eax, msg
+call sprint
+mov eax, esi
+call iprintLF
+ 
+call quit
